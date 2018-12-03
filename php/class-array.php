4c036f90af14064e42d38c503b6c4d3bcf5c2885
Cesartorres12 <cesartorres726@gmail.com>
2018-12-03 18:57:51 -0500

    Add files via upload

---
+++ php/class-array.php
@@ -0,0 +1,22 @@
+<?php
+$a =array(0, 1, 2, 3, 4, 5, 6);
+$a[0] = 'Value 1';
+$a[1] = 'Value 2';
+$a[2] = 'Value 3';
+$a[3] = 'Value 4';
+$a[4] = 'Value 5';
+$a[5] = 'Value 6';
+
+echo "Here we have.$a[2]";
+
+echo '<br>';
+echo '<br>';
+echo '<br>';
+
+$a =array(0, 1, 2, 3, 4, 5, 6);
+
+echo "sum = " . array_sum($a) . "\n";
+
+
+
+ ?>
